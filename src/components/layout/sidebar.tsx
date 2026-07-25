8b3fc00501c313b0a0e08e4713bb139ec44f9c55
Yuge Giri Purboyo <yugegirip@gmail.com>
2026-07-25 11:58:07 +0700

    fix: import useRouter in sidebar.tsx for clean build

--- src/components/layout/sidebar.tsx
+++ src/components/layout/sidebar.tsx
@@ -2,7 +2,7 @@
 
 import Link from 'next/link'
 import Image from 'next/image'
-import { usePathname } from 'next/navigation'
+import { usePathname, useRouter } from 'next/navigation'
 import {
   LayoutDashboard,
   Users,
